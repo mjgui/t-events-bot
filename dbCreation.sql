edc0b6134e2fee4ddf5757faebb7d7f2141a2aca
nicktohzyu <nicholastohzhenyu@gmail.com>
2021-02-24 18:00:39 +0800

    Create dbCreation.sql

---
+++ dbCreation.sql
@@ -0,0 +1,29 @@
+CREATE
+DATABASE "cscEventsBot"
+    WITH
+    OWNER = user1
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+CREATE SCHEMA master
+
+    CREATE TABLE master.variables
+    (
+        "key"   text PRIMARY KEY,
+        "value" text
+    )
+
+    CREATE TABLE master.stations
+    (
+        "stationID" integer PRIMARY KEY, -- corresponds to a table in stations schema
+        name        text COLLATE pg_catalog."default" NOT NULL,
+        "groupID"   bigint                            NOT NULL
+    )
+
+CREATE SCHEMA stations
+
+    CREATE TABLE stations."1" -- put stationID in the blank
+    (
+        "userID"      bigint  PRIMARY KEY,
+        "queueNumber" integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 )
+    )
