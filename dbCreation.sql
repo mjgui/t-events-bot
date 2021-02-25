83f6038804f56c96e6ef29ab058b47b764b06a72
nicktohzyu <nicholastohzhenyu@gmail.com>
2021-02-25 15:06:32 +0800

    Update dbCreation.sql

--- dbCreation.sql
+++ dbCreation.sql
@@ -16,13 +16,21 @@ CREATE SCHEMA master
     CREATE TABLE master.stations
     (
         "stationID" integer PRIMARY KEY, -- corresponds to a table in stations schema
-        name        text COLLATE pg_catalog."default" NOT NULL,
+        "name"        text COLLATE pg_catalog."default" NOT NULL,
+    )
+
+    CREATE TABLE master.participants
+    (
+        "userID"      bigint PRIMARY KEY,
+        "station"       text,
+        "queueNumber" integer NOT NULL,
+        "stationID"   bigint  NOT NULL DEFAULT 0
     )
 
 CREATE SCHEMA stations
 
     CREATE TABLE stations."" -- put stationID in the blank
     (
-        "userID"      bigint  PRIMARY KEY,
+        "userID"      bigint PRIMARY KEY,
         "queueNumber" integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 )
     )
